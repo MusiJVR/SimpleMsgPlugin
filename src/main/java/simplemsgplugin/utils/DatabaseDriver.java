2b9e6408c42f5a7a85f0c6a23161253187e58f42
MusiJVR <mousejava102@gmail.com>
2024-09-20 16:59:36 +0400

    Removed unnecessary imports

--- src/main/java/simplemsgplugin/utils/DatabaseDriver.java
+++ src/main/java/simplemsgplugin/utils/DatabaseDriver.java
@@ -6,7 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
